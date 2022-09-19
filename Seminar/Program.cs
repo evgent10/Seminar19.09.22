a4bd7a17d1a13934df39c876c67571a652b77e95
evgent10 <evgent10@ya.ru>
2022-09-19 22:08:42 +0300

    Задача 1

--- Seminar/Program.cs
+++ Seminar/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a =new Random().Next(100,1000);
+Console.WriteLine(a);
+int c = 0; // первое число
+int b = 0; //последнее
+
+c = a/100 *10;
+ int c1 = a/100;
+b = a%10;
+
+int sum = c+b;
+Console.WriteLine($"{c1} {b}");
+Console.WriteLine(sum);
