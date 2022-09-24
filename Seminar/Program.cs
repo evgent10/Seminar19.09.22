f3331fa98f12031e126c04e82507c3c433ad9376
evgent10 <evgent10@ya.ru>
2022-09-24 22:28:11 +0300

    Задача 10

--- Seminar/Program.cs
+++ Seminar/Program.cs
@@ -1,4 +1,50 @@
-﻿// int a =new Random().Next(100,1000);
+﻿// Задача 10
+//Напишите программу, которая принимает на вход трёхзначное число и 
+//на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трёхзначное число: ");
+int n = int.Parse(Console.ReadLine()!);
+if((n < 100) | (n > 999))  
+    {
+        Console.WriteLine("Не верно написано число");
+        return;
+    }
+    
+int n1 = n/100;
+int n2 = n - n1*100;
+int n3 = n2 % 10;
+int n4 = n2 - n3;
+int n5 = n4 / 10;
+Console.WriteLine($"Вторая цифра {n5}");
+
+
+
+
+
+
+
+
+
+
+
+//     Задача 13
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+// Console.WriteLine("Введите число: ");
+// int a = int.Parse(Console.ReadLine()!);
+// if (a < 100)
+// {
+//     Console.WriteLine("Третей цифры нет");
+// }
+
+
+
+
+
+// int a =new Random().Next(100,1000);
 // Console.WriteLine(a);
 // int c = 0; // первая цифра в числе
 // int b = 0; //последняя цифра в числе
@@ -49,14 +95,14 @@
 // }
 
 // вариант 2
-Console.WriteLine("Введите первое число: ");
-int a = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите первое число: ");
+// int a = int.Parse(Console.ReadLine()!);
 
-if ((a%7 != 0) || (a%23 != 0))
-{
-    Console.WriteLine("Не делится");
-}
-else
-{
-    Console.WriteLine("Введёное число делится на 7 и на 23");
-}
+// if ((a%7 != 0) || (a%23 != 0))
+// {
+//     Console.WriteLine("Не делится");
+// }
+// else
+// {
+//     Console.WriteLine("Введёное число делится на 7 и на 23");
+// }
